55f65853cd725da94eb735d102171d4cc4a827d8
Matteo Piacentini <matteo_piacentini@outlook.it>
2020-05-10 00:33:12 +0200

    Update binstree.h

--- binstree/header/binstree.h
+++ binstree/header/binstree.h
@@ -1,3 +1,5 @@
+
+
 #ifndef BINSTREE_H
 #define BINSTREE_H
 
@@ -5,13 +7,19 @@
 #define DEPTH_PREORDER_VISIT 0
 #define DEPTH_POSTORDER_VISIT 1
 #define DEPTH_INORDER_VISIT 2
-
+#define BREADTH_FIRST_VISIT 3
 
 typedef struct node node;
 
-typedef struct binstree { struct node *radix; } *BSTree;
+typedef struct binstree binstree;
 
-typedef struct pair { unsigned int key; void *value; } *Pair;
+typedef struct pair { 
+    
+    unsigned int key; 
+    
+    void *value; 
+    
+} *Pair;
 
 typedef struct generator generator;
 
@@ -22,6 +30,8 @@ void append(struct binstree *tree, unsigned int key, void* value);
 struct pair* max(struct binstree *tree);
 
 struct pair* min(struct binstree *tree);
+
+unsigned int nnodes(struct binstree *tree);
 
 struct binstree* subtree(struct binstree *tree, unsigned int key);
 
@@ -37,18 +47,37 @@ struct pair* pred(struct binstree *tree, unsigned int key);
 
 void destroyTree(struct binstree *tree); 
 
+int maxdepth(struct binstree *tree);
+
+void balance(struct binstree *tree);
+
+void LLRotation(struct binstree *tree);
+
+void LRRotation(struct binstree *tree);
+
+void RRRotation(struct binstree *tree);
+
+void RLRotation(struct binstree *tree);
+
 struct generator* depth_inorder_iter(struct binstree *tree);
 
 struct generator* depth_preorder_iter(struct binstree *tree);
 
 struct generator* depth_postorder_iter(struct binstree *tree);
 
+struct generator* breadth_first_iter(struct binstree *tree);
+
 struct pair* genNext(struct generator *gen);
 
 struct pair* genCurr(struct generator *gen);
 
-int isGeneratorEmpty(struct generator *gen);
-
 void destroyGenerator(struct generator *gen);
+
+unsigned int* keys(struct binstree *tree, int visit);
+
+void* values(struct binstree *tree, int visit);
+
+struct pair** items(struct binstree *tree, int visit);
+
 
 #endif
